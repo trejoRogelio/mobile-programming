ce1485b8ff0ce179b5b02e3b1697092fa84a3d2e
Jose.Silva <jossilvas85@gmail.com>
2023-10-04 14:17:01 -0600

    merge cambios profe

--- apps/pokedex/src/pages/Home.js
+++ apps/pokedex/src/pages/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Button, StyleSheet, Text, View, Image } from 'react-native';
 import { Link } from 'react-router-native';
 
@@ -26,38 +25,6 @@ const styles = StyleSheet.create({
     width: 200,
     height: 200,
   },
-=======
-import { Button, StyleSheet, Text, View, Image, TextInput } from 'react-native';
-import { Link } from 'react-router-native';
-
-function Home() {
-    return (
-        <View>
-            <View style={styles.main}>
-                <Image source={require('../../assets/pokebola.png')} />
-                <View style={styles.inputs}>
-                    <TextInput style={{ borderWidth: 1 }} placeholder='Search a Pokemon!' />
-                    <Button title='Search' />
-                </View>
-                <View>
-                    <Text>Filters!!!</Text>
-                </View>
-            </View>
-        </View>
-    );
-}
-
-const styles = StyleSheet.create({
-    main: {
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    inputs: {
-        width: 400,
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-    }
->>>>>>> 9eba55971e178c6907ee037230a7f9156f75cec9
 });
 
 export default Home;
--- apps/pokedex/src/pages/Information.js
+++ apps/pokedex/src/pages/Information.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Button, Text, View } from 'react-native';
 import { Link } from 'react-router-native';
 
@@ -11,21 +10,6 @@ function Information() {
       </Link>
     </View>
   );
-=======
-import { Button, Text, View, } from 'react-native';
-import { Link } from 'react-router-native';
-
-function Information() {
-    return (
-        <View>
-            <Text>Information Page</Text>
-
-            <Link to='/'>
-                <Text> Go To Home!!!</Text>
-            </Link>
-        </View>
-    );
->>>>>>> 9eba55971e178c6907ee037230a7f9156f75cec9
 }
 
 export default Information;
