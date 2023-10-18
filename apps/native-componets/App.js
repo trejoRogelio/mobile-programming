2d303a84770c60237357919b7e945e36c9295235
César Alejandro Hernández Macías <up200775@alumnos.upa.edu.mx>
2023-10-18 11:40:38 -0600

    andor en mi branch

--- apps/native-componets/App.js
+++ apps/native-componets/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Hernández Macías César Alejandro
+
 export default function App() {
   return (
     <View style={styles.container}>
