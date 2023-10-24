a292a91a88730568cdd4915a0a6d9109f57ce2b5
Derek Ramirez <up200424@alumnos.upa.edu.mx>
2023-10-24 14:36:41 -0600

    native components

--- apps/native-componets/App.js
+++ apps/native-componets/App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert, Button } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 export default function App() {
+  const HasbullaImg =  'https://cdn.milenio.com/uploads/media/2022/10/13/hasbulla-instagram.jpg';
+  const [imageUri, setImageUri] = useState(HasbullaImg);
+  const handlePress = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync();
+    if (result.canceled) {
+      Alert.alert('Dennied permission');
+      return;
+    }
+    setImageUri(result.assets[0].uri)
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Select an image</Text>
+      <Image
+        width={200}
+        height={200}
+        style={{ margin: 20 }}
+        source={{
+          uri: imageUri
+        }}
+      />
+      <Button title="Open gallery" onPress={handlePress} />
       <StatusBar style="auto" />
     </View>
   );
