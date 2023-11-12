753f1bd63ed0eb56c4ad79e28d01600af440540a
Derek Ramirez <up200424@alumnos.upa.edu.mx>
2023-11-12 17:15:09 -0600

    update

--- apps/todo-list-app/src/pages/Home.tsx
+++ apps/todo-list-app/src/pages/Home.tsx
@@ -1,10 +1,23 @@
 import {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar, IonGrid, IonRow, IonCol, IonButton, IonInput, IonAlert, IonList, IonItem, IonLabel, IonCheckbox
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonInput,
+  IonAlert,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonCheckbox,
+  IonIcon,
 } from '@ionic/react';
+
+import { trash, addCircle } from 'ionicons/icons';
 
 import { type FormEvent } from 'react';
 
@@ -12,83 +25,135 @@ import './Home.css';
 import { useState } from 'react';
 
 type Todo = {
-    id: number;
-    todo: string;
-    done: boolean;
+  id: number;
+  todo: string;
+  done: boolean;
 };
 
 const Home: React.FC = () => {
-    const [todo, setTodo] = useState("");
-    const [todos, setTodos] = useState<Todo[]>([
-        { id: new Date().getTime(), todo: "I have to run", done: true }
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: new Date().getTime(), todo: 'I have to run', done: true },
+  ]);
+
+  const handdleInput = (e: Event): void => {
+    const value = (e.target as HTMLIonInputElement).value;
+    setTodo(value as string);
+  };
+
+  const handleClick = () => {
+    setTodos((allTodos) => [
+      ...allTodos,
+      {
+        id: new Date().getTime(),
+        done: false,
+        todo,
+      },
     ]);
+    setTodo('');
+  };
 
-    const handdleInput = (e: FormEvent): void => {
-        const value = (e.target as HTMLIonInputElement).value;
-        setTodo(value as string);
-    }
+  const handleDelete = (id: number) => {
+    setTodos((allTasks) => {
+      const filteredTasks = allTasks.filter((item) => item.id !== id);
+      return filteredTasks;
+    });
+  };
 
-    const handleClick = () => {
-        setTodos((allTodos) => [...allTodos, {
-            id: new Date().getTime(),
-            done: false,
-            todo
-        }])
-        setTodo("")
-    }
+  const handleDeleteAllSelected = () => {
+    setTodos((allTasks) => {
+      const filteredTasks = allTasks.filter((task) => !task.done);
+      return filteredTasks;
+    });
+  };
 
-    const handdleClickCheckBox = (event: FormEvent, id: number) => {
-        const value = (event.target as HTMLIonCheckboxElement).checked;
-        const findTodoindex = todos.findIndex((i) => i.id === id)!;
-        setTodos((allTodos) => {
-            allTodos[findTodoindex].done = value;
-            return [...allTodos];
-        });
-    }
+  const handdleClickCheckBox = (event: FormEvent, id: number) => {
+    const value = (event.target as HTMLIonCheckboxElement).checked;
+    const findTodoindex = todos.findIndex((i) => i.id === id)!;
+    setTodos((allTodos) => {
+      allTodos[findTodoindex].done = value;
+      return [...allTodos];
+    });
+  };
 
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Todo List</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen>
-                <IonGrid>
-                    <IonRow>
-                        <IonCol>
-                            <IonInput
-                                type='text'
-                                placeholder='Write a todo'
-                                onInput={handdleInput}
-                                value={todo}
-                            />
-                        </IonCol>
-                        <IonCol>
-                            <IonButton onClick={handleClick}>Add</IonButton>
-                            {/* <IonAlert
-                                trigger="button"
-                                message={todo}
-                                buttons={['OK', 'Nell']}
-                            /> */}
-                        </IonCol>
-                    </IonRow>
-                </IonGrid>
-                <IonList>
-                    {
-                        todos.map((item) => (
-                            <IonItem key={item.id}>
-                                <IonCheckbox onClick={(e) => handdleClickCheckBox(e, item.id)} checked={item.done}>
-                                    <IonLabel>{item.todo}</IonLabel>
-                                </IonCheckbox>
-                            </IonItem>
-                        ))
-                    }
-                </IonList>
-            </IonContent>
-
-        </IonPage>
-    );
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Todo List</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonInput
+                type="text"
+                placeholder="Write a todo"
+                onIonInput={handdleInput}
+                value={todo}
+              />
+            </IonCol>
+            <IonCol>
+              <IonButton onIonFocus={handleClick}>
+                <IonIcon icon={addCircle} />
+              </IonButton>
+              <IonButton id="delete-all">
+                <IonIcon icon={trash} />
+              </IonButton>
+              <IonAlert
+                trigger={`delete-all`}
+                header="confirm"
+                message="you will delete this item"
+                buttons={[
+                  { role: 'cancel', text: 'Cancel' },
+                  {
+                    role: 'confirm',
+                    text: 'Confirm',
+                    handler: handleDeleteAllSelected,
+                  },
+                ]}
+              />
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+        <IonList>
+          {todos.map((item) => (
+            <IonItem key={item.id}>
+              <IonCheckbox
+                onClick={(e) => handdleClickCheckBox(e, item.id)}
+                checked={item.done}
+              >
+                <IonLabel>{item.todo}</IonLabel>{' '}
+              </IonCheckbox>
+              <IonButton
+                id={`delete-item-${item.id}`}
+                style={{ margin: 10 }}
+                color={'danger'}
+              >
+                Delete
+              </IonButton>
+              <IonAlert
+                trigger={`delete-item-${item.id}`}
+                header="confirm"
+                message="you will delete this item"
+                buttons={[
+                  { role: 'cancel', text: 'Cancel' },
+                  {
+                    role: 'confirm',
+                    text: 'Confirm',
+                    handler: () => {
+                      handleDelete(item.id);
+                    },
+                  },
+                ]}
+              />
+            </IonItem>
+          ))}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Home;
