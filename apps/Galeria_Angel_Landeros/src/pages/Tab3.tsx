3e0e7ef2af0c342865864390b4b3fd44fae9284f
Angel Landeros <up200518@alumnos.upa.edu.mx>
2023-11-21 21:03:35 -0600

    Galeria Landeros

---
+++ apps/Galeria_Angel_Landeros/src/pages/Tab3.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonImg, IonActionSheet } from '@ionic/react';
+import { trash, close } from 'ionicons/icons';
+import { usePhotoGallery, UserPhoto } from '../hooks/usePhotoGallery';
+
+const Tab3: React.FC = () => {
+  const { deletePhoto, photos } = usePhotoGallery();
+  const [photoToDelete, setPhotoToDelete] = useState<UserPhoto>();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg onClick={() => setPhotoToDelete(photo)} src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+
+        <IonActionSheet
+          isOpen={!!photoToDelete}
+          buttons={[{
+            text: 'Delete',
+            role: 'destructive',
+            icon: trash,
+            handler: () => {
+              if (photoToDelete) {
+                deletePhoto(photoToDelete);
+                setPhotoToDelete(undefined);
+              }
+            }
+          }, {
+            text: 'Cancel',
+            icon: close,
+            role: 'cancel'
+          }]}
+          onDidDismiss={() => setPhotoToDelete(undefined)}
+        />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab3;
--- apps/pokedex/src/components/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ActivityIndicator } from 'react-native';
-
-const Loading = () => {
-  return <ActivityIndicator style={{ width: 'auto', height: 250 }} size='large' color='#E53939' />;
-};
-
-export default Loading;
